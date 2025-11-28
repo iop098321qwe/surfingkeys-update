fc6885c7d1f2e51db7ab16d3f9089d6b72c40a2b
Dallas Elliott <iop098321qwe@pm.me>
2025-11-28 13:28:10 -0700

    commit

---
+++ cbc-module.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# cp ./catppuccin-mocha-surfingkeys.js ~/.config/surfingkeys/catppuccin-mocha-surfingkeys.js
+
+# Copy the catppuccin-mocha-surfingkeys.js file to the SurfingKeys configuration directory with confirmation using gum
+sktu() {
+  gum confirm "Do you want to update the SurfingKeys configuration with Catppuccin Mocha theme?" &&
+    cp ./catppuccin-mocha-surfingkeys.js ~/.config/surfingkeys/catppuccin-mocha-surfingkeys.js &&
+    gum style --foreground '#89b4fa' "SurfingKeys configuration updated successfully!" &&
+    gum style --foreground '#f9e2af' "Don't forget to input '/home/grymm/.config/surfingkeys/catppuccin-mocha-surfingkeys.js' as the config location in SurfingKeys extension." ||
+    gum style --foreground '#f38ba8' "Update canceled."
+}
